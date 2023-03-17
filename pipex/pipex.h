3c7dfb88c1bb38223c80cec26b820eaf68f482bf
loudapet <peta.louda95@gmail.com>
2023-03-17 10:17:04 +0100

    Headers for allowed functions

---
+++ pipex/pipex.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: plouda <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/15 08:31:05 by plouda            #+#    #+#             */
+/*   Updated: 2023/03/15 12:48:39 by plouda           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <sys/wait.h>
+# include <fcntl.h>
+# include <string.h>
+# include "libftprintf/ft_printf.h"
+
+
+#endif
