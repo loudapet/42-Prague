b81dd8b5963c5655fc3169a0a17c6b5982939253
loudapet <peta.louda95@gmail.com>
2023-05-15 09:18:03 +0200

    Added instruction background, helper functions for deg to rad conv

--- fdf/src/utils.c
+++ fdf/src/utils.c
@@ -3,14 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: plouda <plouda@student.42.fr>              +#+  +:+       +#+        */
+/*   By: plouda <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/20 11:09:17 by plouda            #+#    #+#             */
-/*   Updated: 2023/05/12 13:12:00 by plouda           ###   ########.fr       */
+/*   Updated: 2023/05/14 16:37:16 by plouda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
+
+double	rad_to_deg(double rad)
+{
+	double	deg;
+
+	deg = rad * 180/M_PI;
+	return (deg);
+}
+
+double	deg_to_rad(double deg)
+{
+	double	rad;
+
+	rad = deg * M_PI/180;
+	return (rad);
+}
 
 int	abs_val(int nb)
 {
@@ -45,18 +61,29 @@ void	reset_img(mlx_image_t *img)
 	u_int32_t	x;
 	u_int32_t	y;
 
-	color = get_rgba(
-			1 % 0xFF, // R
-			1 % 0xFF, // G
-			1 % 0xFF, // B
-			254 % 0xFF  // A
-		);
+	color = 0x000000ff;
 	x = 0;
 	while (x < img->width)
 	{
 		y = 0;
 		while (y < img->height)
 		{
+			mlx_put_pixel(img, x, y, color);
+			y++;
+		}
+		x++;
+	}
+	x = 0;
+	while (x < 300)
+	{
+		color = 0x202020ff;
+		if (x == 299)
+			color = 0xffffffff;
+		y = 0;
+		while (y < 160)
+		{
+			if (y == 159)
+				color = 0xffffffff;
 			mlx_put_pixel(img, x, y, color);
 			y++;
 		}
