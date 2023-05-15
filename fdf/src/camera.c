deb004f68ad37f624a8b2f9f8bcbe34ac9049d3f
loudapet <peta.louda95@gmail.com>
2023-05-15 09:16:06 +0200

    Added z-div, fixed typecast

--- fdf/src/camera.c
+++ fdf/src/camera.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   camera.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: plouda <plouda@student.42.fr>              +#+  +:+       +#+        */
+/*   By: plouda <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 10:59:30 by plouda            #+#    #+#             */
-/*   Updated: 2023/05/09 11:00:01 by plouda           ###   ########.fr       */
+/*   Updated: 2023/05/13 22:50:43 by plouda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,10 @@ t_camera	*init_camera(t_master *master)
 	camera->gamma = 0;
 	camera->x_offset = 0;
 	camera->y_offset = 0;
+	camera->z_div = 1;
 	if (master->mlx->width / master->vmap->ncols / 2 > master->mlx->height / master->vmap->nrows / 2)
-		camera->zoom = master->mlx->height / master->vmap->nrows / 2;
+		camera->zoom = (double)(master->mlx->height / master->vmap->nrows / 2);
 	else
-		camera->zoom = master->mlx->width / master->vmap->ncols / 2;
+		camera->zoom = (double)(master->mlx->width / master->vmap->ncols / 2);
 	return (camera);
 }
