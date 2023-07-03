741570af6c7178b39f59ae763e48db6be99d65ee
Petr Louda <plouda@student.42prague.com>
2023-07-03 14:24:57 +0200

    First minishell commit, easy prompt

---
+++ minishell/src/project/main.c
@@ -0,0 +1,100 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: plouda <plouda@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/03 10:15:35 by plouda            #+#    #+#             */
+/*   Updated: 2023/07/03 14:21:10 by plouda           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+size_t	get_hostname_len(char *hostname)
+{
+	size_t	i;
+
+	i = 0;
+	while (hostname[i] != '.' && hostname[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*get_hostname(void)
+{
+	int		fd;
+	char	*hostname_line;
+	char	*hostname_raw;
+	char	*hostname;
+
+	fd = open("/etc/hostname", O_RDONLY);
+	hostname_line = get_next_line(fd);
+	hostname_raw = ft_substr(hostname_line, 0, get_hostname_len(hostname_line));
+	hostname = ft_strjoin(hostname_raw, ":");
+	free(hostname_line);
+	free(hostname_raw);
+	return (hostname);
+}
+
+char	*get_username(void)
+{
+	char	*username;
+
+	username = getenv("USER");
+	username = ft_strjoin(username, "@");
+	return (username);
+}
+
+char	*get_directory(void)
+{
+	char	*abs;
+	char	*home;
+	char	*username;
+	char	*home_dollar;
+	int		i;
+
+	i = 0;
+	username = getenv("USER");
+	abs = getcwd(NULL, 0);
+	home = ft_strnstr(abs, username, ft_strlen(abs));
+	home = home + ft_strlen(username);
+	home = ft_strjoin("~", home);
+	home_dollar = ft_strjoin(home, "$ ");
+	free(abs);
+	free(home);
+	return (home_dollar);
+}
+
+int	main(void)
+{
+	char	*line;
+	char	*username;
+	char	*hostname;
+	char	*specs;
+	char	*dir;
+	char	*prompt;
+
+	username = get_username();
+	hostname = get_hostname();
+	dir = get_directory();
+	specs = ft_strjoin(username, hostname);
+	prompt = ft_strjoin(specs, dir);
+	while (1)
+	{
+		line = readline((const char *)prompt);
+		add_history(line);
+		if (!ft_strncmp(line, "q", ft_strlen(line)))
+			break ;
+		free(line);
+	}
+	free(line);
+	free(username);
+	free(hostname);
+	free(specs);
+	free(dir);
+	free(prompt);
+	write(1, "Goodbye!\n", 10);
+	return (0);
+}
